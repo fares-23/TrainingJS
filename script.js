252cf0bf10915392600b94dc41e8020b596e5af6
fares-23 <joggings.estival0m@icloud.com>
2023-06-30 09:20:25 +0200

    if et prompt

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+let motTapeOk = true // Essayez de mettre false à la place de true
+
+if (motTapeOk) {
+    console.log("Bravo, vous avez correctement tapé le mot")
+} else {
+    console.log("Échec, le mot n'est pas correct")
+}
+
+let motUtilisateur = prompt("entrer un mot : ");
+console.log(motUtilisateur);
