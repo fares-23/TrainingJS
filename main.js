bcaef4cffe29fe3df0b0d8ffccf40ac1fc84ada4
fares-23 <joggings.estival0m@icloud.com>
2023-07-01 20:40:58 +0200

    interaction entre HTML et JS avec ciblage d'un élément et création d'un

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 
 
-//lancerJeu();
-document.addEventListener("DOMContentLoaded",function(){
-    let listeInputRadio = document.querySelectorAll(".zoneChoix input");
-    console.log(listeInputRadio);
-})
+lancerJeu();
+/*
+let contenuTitre = "Azertype"
+let contenuParagraphe = "L'application pour apprendre à taper plus vite !"
+
+//creation div, dans laquelle on va vouloir mettre le titre et la paragraphe
+let nouvelleDiv = document.createElement("div")
+let nouveauTitre = document.createElement("h1")
+let nouveauParagraphe = document.createElement("p")
+
+//ajout text et titre 
+nouveauTitre.textContent = contenuTitre
+nouveauParagraphe.textContent = contenuParagraphe
+
+//ajout titre et paragraphe dans la div
+nouvelleDiv.appendChild(nouveauTitre)
+nouvelleDiv.appendChild(nouveauParagraphe)
+
+//ajout de la div dans le body
+let body = document.querySelector("body")
+body.appendChild(nouvelleDiv)
+*/
+
 
 
 
--- script.js
+++ script.js
@@ -1,46 +1,48 @@
 let score = 0;
-let nbElements;
+let nbElements =0 ;
 
 
 function afficherResultat(){
-    console.log('votre score est de ' + this.score + ' sur ' + nbElements);
+    let spanScore = document.querySelector(".zoneScore span")
+    let resultat = 
+    `
+        ${score}/${nbElements}
+    `
+    spanScore.innerHTML = resultat
 }
 
 function choisirPhraseOuMots(){
     let choix;
     do{
         choix = prompt("choisir 'mots' ou 'phrase' en écrivant le choix");
-        console.log(choix);
     }while(choix != 'mots' && choix != 'phrase');
     return choix;
 }
 
 
 function lancerBoucleDeJeu(choix,listeMots,listePhrases){
-    switch(choix){
-        case 'mots':
-            for(let i in listeMots){
-                if(prompt("copier le mot " + listeMots[i]) == listeMots[i]){
-                    score++;
-                }
+    if(choix === 'mots'){
+        for(let i in listeMots){
+            if(prompt("copier le mot : " + listeMots[i]) == listeMots[i]){
+                score++;
             }
-            nbElements=listeMots.length;
-            break;
-        case 'phrase':
-            for(let i in listePhrases){
-                if(prompt("copier le mot " + listePhrases[i]) == listePhrases[i]){
-                    score++;
-                }
+        }
+        nbElements=listeMots.length;
+    }else{
+        for(let i in listePhrases){
+            if(prompt("copier la phrase : " + listePhrases[i]) == listePhrases[i]){
+                score++;
             }
-            nbElements=listePhrases.length;
-            break;
-    }  
-    console.log('lancerboucle')     
+        }
+        nbElements=listePhrases.length;
+    }     
 }
 
 function lancerJeu(){
     lancerBoucleDeJeu(choisirPhraseOuMots(),listeMots,listePhrases);
     afficherResultat();
 }
+
+
 
 
