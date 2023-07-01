e9699d464418e5e062d80c8680bf130f3e608904
fares-23 <joggings.estival0m@icloud.com>
2023-07-01 21:14:14 +0200

    cleaning du code

--- script.js
+++ script.js
@@ -11,37 +11,6 @@ function afficherResultat(){
     spanScore.innerHTML = resultat
 }
 
-function choisirPhraseOuMots(){
-    let choix;
-    do{
-        choix = prompt("choisir 'mots' ou 'phrase' en écrivant le choix");
-    }while(choix != 'mots' && choix != 'phrase');
-    return choix;
-}
-
-
-function lancerBoucleDeJeu(choix,listeMots,listePhrases){
-    if(choix === 'mots'){
-        for(let i in listeMots){
-            if(prompt("copier le mot : " + listeMots[i]) == listeMots[i]){
-                score++;
-            }
-        }
-        nbElements=listeMots.length;
-    }else{
-        for(let i in listePhrases){
-            if(prompt("copier la phrase : " + listePhrases[i]) == listePhrases[i]){
-                score++;
-            }
-        }
-        nbElements=listePhrases.length;
-    }     
-}
-
-function lancerJeu(){
-    lancerBoucleDeJeu(choisirPhraseOuMots(),listeMots,listePhrases);
-    afficherResultat();
-}
 
 
 
