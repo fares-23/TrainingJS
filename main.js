9a0648a56d6b05411eab06c7bc58ea576f754fe9
fares-23 <joggings.estival0m@icloud.com>
2023-07-01 13:07:29 +0200

    ajout document.addEventListener qui permet de charger le DOM

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
 
 //lancerJeu();
+document.addEventListener("DOMContentLoaded",function(){
+    let listeInputRadio = document.querySelectorAll(".zoneChoix input");
+    console.log(listeInputRadio);
+})
 
-let listeInputRadio = document.querySelectorAll(".zoneChoix input");
-console.log(listeInputRadio);
+
+
